07d0d691ea7a2d4831ce90f2d49b5648e950728c
Clóvis Neto <cloves13@hotmail.com>
2019-07-16 23:56:20 +0200

    Create hash table data structure

---
+++ data-structures/hash-table/hash-table.js
@@ -0,0 +1,60 @@
+const HashTable = function() {
+  let storage = [];
+  const storageLimit = 14;
+
+  this.print = function() {
+    console.log(storage);
+  };
+
+  this.hash = function(string, max) {
+    var hash = 0;
+    for (var i = 0; i < string.length; i++) {
+      hash += string.charCodeAt(i);
+    }
+    return hash % max;
+  };
+
+  this.add = function(key, value) {
+    var index = this.hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      storage[index] = [[key, value]];
+    } else {
+      var inserted = false;
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index][i][1] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        storage[index].push([key, value]);
+      }
+    }
+  };
+
+  this.remove = function(key) {
+    var index = this.hash(key, storageLimit);
+    if (storage[index].length === 1 && storage[index][0][0] === key) {
+      delete storage[index];
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          delete storage[index][i];
+        }
+      }
+    }
+  };
+
+  this.lookup = function(key) {
+    var index = this.hash(key, storageLimit);
+    if (storage[index] === undefined) {
+      return undefined;
+    } else {
+      for (var i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
+  };
+};
