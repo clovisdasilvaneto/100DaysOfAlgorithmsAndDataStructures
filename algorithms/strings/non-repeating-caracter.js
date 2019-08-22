c73ef278f952b080aa5cdb9c348baba687bf6c5b
Clóvis Neto <cloves13@hotmail.com>
2019-08-23 00:33:40 +0200

    Solve non repeatable carachter problem

---
+++ algorithms/strings/non-repeating-caracter.js
@@ -0,0 +1,29 @@
+const findSingleCaracter = word => {
+  const hashMap = {};
+
+  for (let i = 0; i < word.length; i++) {
+    if (hashMap[word[i]]) {
+      delete hashMap[word[i]];
+    } else {
+      hashMap[word[i]] = true;
+    }
+  }
+
+  for (let prop in hashMap) {
+    hashMap.single = prop;
+    break;
+  }
+
+  return hashMap.single;
+};
+
+const a = "aabbc";
+const b = "aazycc";
+const c = "aabb";
+
+//If there's more than one non_repeatable, you should
+//show only the first one 👍
+
+console.log("a: ", findSingleCaracter(a));
+console.log("b: ", findSingleCaracter(b));
+console.log("c: ", findSingleCaracter(c));
